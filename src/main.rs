343c49d8d642bd5bd5cc99d1d1f71f56f716ddb0
nkxxll <niki.nice1203@gmail.com>
2025-05-08 15:38:07 +0200

    get and interactive

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
 use std::io::BufWriter;
+use std::ops::Sub;
 use std::sync::Arc;
 use std::sync::atomic::Ordering;
-use std::thread::{self, sleep};
+use std::sync::mpsc::{self, Sender};
+use std::thread::{self};
 use std::time::Duration;
 use std::{io::Write, sync::atomic::AtomicBool};
 
@@ -12,6 +14,7 @@ use reqwest::Client;
 use reqwest::Response;
 
 const OUT_FILE: &str = "rget.out";
+const DEFAULT_DEPTH: usize = 1;
 
 /// Simple program to download a URL
 #[derive(Parser, Debug)]
@@ -31,61 +34,86 @@ enum SubCom {
         outfile: String,
     },
     /// start the program in interactive mode
-    Interactive,
+    Interactive {
+        #[arg(short, long, default_value = OUT_FILE)]
+        outfile: String,
+    },
+    GetDepth {
+        /// The URL to download
+        url: String,
+        #[arg(short, long, default_value_t = DEFAULT_DEPTH)]
+        depth: usize,
+    },
 }
 
 struct Spinner {
     chars: Vec<char>,
-    ab: Arc<AtomicBool>,
+    stop_tx: Option<Sender<bool>>,
 }
 
 impl Spinner {
-    fn new(ab: Arc<AtomicBool>) -> Self {
-        let chars = vec!['-', '\\', '|', '/'];
-        Spinner { chars, ab }
+    fn new(chars: Option<Vec<char>>) -> Self {
+        match chars {
+            Some(ch) => Spinner {
+                chars: ch,
+                stop_tx: None,
+            },
+            None => {
+                let chars = vec!['-', '\\', '|', '/'];
+                Spinner {
+                    chars,
+                    stop_tx: None,
+                }
+            }
+        }
     }
 
-    fn start(self) -> std::thread::JoinHandle<()> {
+    fn start(&mut self) -> thread::JoinHandle<()> {
+        let (tx, rx) = mpsc::channel::<bool>();
+        let chars = self.chars.clone();
+        self.stop_tx = Some(tx);
+
         thread::spawn(move || {
             let mut i = 0;
-            while !self.ab.load(Ordering::Relaxed) {
-                print!("\r{}", self.chars[i]);
+            loop {
+                if rx.try_recv().is_ok_and(|x| x) {
+                    break;
+                }
+
+                print!("\r{}", chars[i]);
                 std::io::Write::flush(&mut std::io::stdout()).unwrap();
                 thread::sleep(Duration::from_millis(100));
-                i = (i + 1) % self.chars.len();
+                i = (i + 1) % chars.len();
             }
             println!("\rDone!        ");
         })
     }
 
-    fn stop(self) {
-        self.ab.store(true, Ordering::Relaxed)
+    fn stop(&mut self) {
+        // Dropping the sender stops the spinner
+        self.stop_tx.as_ref().unwrap().send(true).unwrap();
+        self.stop_tx = None;
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let ab = Arc::new(AtomicBool::new(false));
-    let clone = Arc::clone(&ab);
-    let sp = Spinner::new(ab);
 
-    sp.start();
     match &args.subs {
-        SubCom::Interactive => {
-            sleep(Duration::from_secs(1));
-            clone.store(true, Ordering::Relaxed);
-            return loop_download().await;
+        SubCom::Interactive { outfile } => {
+            return loop_download(outfile).await;
         }
         SubCom::Get { url, outfile } => {
-            sleep(Duration::from_secs(1));
-            clone.store(true, Ordering::Relaxed);
             return download(url, outfile).await;
+        }
+        SubCom::GetDepth { url, depth } => {
+            todo!("still needs to be implemented")
         }
     }
 }
 
-async fn loop_download() -> Result<(), Box<dyn std::error::Error>> {
+async fn loop_download(outfile: &str) -> Result<(), Box<dyn std::error::Error>> {
     loop {
         let mut buf = String::new();
         print!("> ");
@@ -99,7 +127,7 @@ async fn loop_download() -> Result<(), Box<dyn std::error::Error>> {
 
         let of = match split.next() {
             Some(outfile) => outfile,
-            None => OUT_FILE,
+            None => outfile,
         };
 
         if url == "quit" || url == "q" {
@@ -120,10 +148,18 @@ async fn loop_download() -> Result<(), Box<dyn std::error::Error>> {
 async fn download(url: &str, outfile: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut response = Client::new().get(url).send().await?.error_for_status()?;
 
-    let total_size = response
-        .content_length()
-        .ok_or("Failed to get content length")?;
+    let total_size = response.content_length();
+    match total_size {
+        Some(ts) => download_pb(outfile, ts, &mut response).await,
+        None => download_sp(outfile, response).await,
+    }
+}
 
+async fn download_pb(
+    outfile: &str,
+    total_size: u64,
+    response: &mut Response,
+) -> Result<(), Box<dyn std::error::Error>> {
     let pb = ProgressBar::new(total_size);
     pb.set_style(
         ProgressStyle::with_template(
@@ -135,14 +171,31 @@ async fn download(url: &str, outfile: &str) -> Result<(), Box<dyn std::error::Er
 
     let mut dest = BufWriter::new(File::create(outfile)?);
 
-    let mut downloaded: usize = 0;
+    let mut downloaded: u64 = 0;
 
     while let Some(chunk) = response.chunk().await? {
         dest.write_all(chunk.as_ref())?;
-        downloaded += chunk.len();
-        pb.set_position(downloaded as u64);
+        downloaded += chunk.len() as u64;
+        pb.set_position(downloaded);
     }
 
     pb.finish_with_message("Download complete");
+    Ok(())
+}
+
+async fn download_sp(outfile: &str, response: Response) -> Result<(), Box<dyn std::error::Error>> {
+    let ab = Arc::new(AtomicBool::new(false));
+    let clone = Arc::clone(&ab);
+    let mut sp = Spinner::new(None);
+
+    let handle = sp.start();
+    let mut outfile = File::create(outfile)?;
+
+    let content = response.bytes().await?;
+    outfile.write_all(&content)?;
+    clone.store(true, Ordering::Relaxed);
+    sp.stop();
+    handle.join().unwrap();
+
     Ok(())
 }
